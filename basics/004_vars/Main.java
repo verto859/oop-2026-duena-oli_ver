7ccf889a3b1319de06ed7740431393b6fc07dc17
John Aldrich Bernardo <4ldrich@protonmail.com>
2025-07-04 00:41:43 +0800

    fix typos

--- basics/004_vars/Main.java
+++ basics/004_vars/Main.java
@@ -1,10 +1,10 @@
 public class Main {
-  public static void main(String[] args) {
-      int num1 = 10;
-      int num2 = 20;
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
 
-      // num1 + num2 = 30
-      // `30` should be our sum, but there something's is wrong here
-      System.out.println("num1 + num2 = " + num1 + num2);
-  }
+        // num1 + num2 = 30
+        // `30` should be our sum, but something is wrong here
+        System.out.println("num1 + num2 = " + num1 + num2);
+    }
 }
