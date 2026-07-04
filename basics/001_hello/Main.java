570a71dad557c3c1a63d4f008e3ba089a1617191
Duena, Oliver A. <oliverduena27@gmail.com>
2026-07-04 04:13:01 +0000

    hello

--- basics/001_hello/Main.java
+++ basics/001_hello/Main.java
@@ -21,7 +21,7 @@
 
 public class Main {
 
-    public static void Main(String[] args) {
-        {???}.out.print("Hello World!");
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
     }
 }
